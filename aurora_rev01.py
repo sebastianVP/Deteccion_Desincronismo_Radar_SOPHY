0f20685a8e7c42767239d53f3ada1395b513b4ce
sebastianVP <alex.valdezp22@gmail.com>
2024-02-27 10:14:08 -0500

    integracion de programas PRUEBA 1

---
+++ aurora_rev01.py
@@ -0,0 +1,11 @@
+# PROGRAMA AURORA ENCARGADO DE :
+'''
+- Busqueda de un directorio especifico
+- Lectura de archivos procesados.
+- Configurado para ejecutarse cada 15 minutos.
+- Revision de archivos de reflectividad.
+- Implementacion de metodo de deteccion de desincronismo.
+- Reinicio de experimento usando el SIR Web.
+'''
+
+import requests
